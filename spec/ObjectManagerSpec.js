6abf6a3ccd893d4bb71b655ba335b8e8dab00b3d
Bart van den Burg <bart@burgov.nl>
2014-03-11 11:54:32 +0100

    added some tests

--- spec/ObjectManagerSpec.js
+++ spec/ObjectManagerSpec.js
@@ -34,15 +34,39 @@ describe('instantiate new object', function() {
 
     it('will load a proxy object if the data of a many2one is unknown', function() {
         expect(project.type).toBeDefined(project.type.object);
+        expect(function() {
+            project.type.name;
+        }).toThrow(Error("Entity cannot be loaded"));
         expect(project.type.id).toBe(8);
         om.create(ProjectType, { id: 8, name: "Hello" });
         expect(project.type.name).toBe('Hello');
+    });
+
+    it('will throw an error on trying to fetch an unloaded entity', function() {
+        expect(function() {
+            om.get(Project, 100);
+        }).toThrow(Error("not loaded"));
+    });
+
+    it('will throw an error on trying to access an unloaded Proxy property', function() {
+        expect(function() {
+            project.type.name;
+        }).toThrow(Error("Entity cannot be loaded"));
     });
 
     it('will understand a relation is the same if it is loaded twice', function() {
         expect(project2.type).toBeInstanceOf(ProjectType);
         expect(project2.type.id).toBe(5);
         expect(project2.type === project3.type).toBeTruthy();
+    });
+
+    it('will update the data of an already existing entity', function() {
+        expect(project.name).toBe('Project name');
+        expect(project.type.id).toBe(8);
+        om.create(Project, { id: 10, name: 'New project name', created_at: '2011-05-06T12:00:00Z', type: 5 });
+        expect(project.name).toBe('New project name');
+        expect(project.type.id).toBe(5);
+        expect(project.type.name).toBe('Type 5');
     });
 
 })
